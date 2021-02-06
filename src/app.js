d3a782a6da8a25009a2fa339debf20853ed2589d
Cheruku Deeksha <deeksha.cheruku@gmail.com>
2021-02-06 19:50:58 +0530

    1517970

--- src/app.js
+++ src/app.js
@@ -27,7 +27,18 @@ const initPlayers = (players) => {
 
     // Instead of forloop use Map method
     // Code here
-
+    detailedPlayers=players.map((data,i)=>{
+        obj={
+            name:data,
+            strength:getRandomStrength(),
+            image:"images/super-"+(i+1)+".png ",
+        }
+        if(i%2 == 0)
+            obj['type'] = 'hero'
+        else
+            obj['type'] ='villain'
+        return obj;
+    })
     return detailedPlayers;
 }
 
@@ -38,11 +49,18 @@ const getRandomStrength = () => {
 
 // Build player template
 const buildPlayers = (players, type) => {
-    let fragment = '';
+    
 
     // Instead of using for loop
     // Use chaining of Array methods - filter, map and join
     // Type your code here
+
+    let fragment = players.filter(player => player.type == type).map(
+        player => `<div class="player">
+        <img src="${player.image}"alt="">
+        <div class="name">${player.name}</div>
+        <div class="strength">${player.strength}</div>
+        </div>`).join();
 
     return fragment;
 }
